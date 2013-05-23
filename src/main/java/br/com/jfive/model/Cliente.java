ac136d7b4de02cfb9eb59a1e19686eec639d438b
Rodrigo Almeida <dgrodrigo@gmail.com>
2013-05-23 20:45:41 -0300

    criando primeiro model

---
+++ src/main/java/br/com/jfive/model/Cliente.java
@@ -0,0 +1,11 @@
+package br.com.jfive.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rodrigoalmeida
+ * Date: 23/05/13
+ * Time: 20:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cliente {
+}
