379db6c7116c8ed38433c10a07ce9c66ac53d9a3
Rodrigo Almeida <dgrodrigo@gmail.com>
2013-05-23 20:47:13 -0300

    teste

---
+++ src/test/java/br/com/jfive/fixture/Teste.java
@@ -0,0 +1,11 @@
+package br.com.jfive.fixture;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rodrigoalmeida
+ * Date: 23/05/13
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class Teste {
+}
